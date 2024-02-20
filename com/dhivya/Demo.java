42cb8ed720d9e8961fe894aefd00bbd0e089fdc2
unknown <dhivya3992@gmail.com>
2024-02-20 20:56:58 +0530

    new codes

---
+++ com/dhivya/Demo.java
@@ -0,0 +1,23 @@
+package com.dhivya;
+
+public class Demo {
+public static void main(String[] args) {
+	
+	
+	
+	
+	System.out.println("this is demo");
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ com/dhivya/Dhivya.java
@@ -0,0 +1,30 @@
+package com.dhivya;
+
+public class Dhivya {
+
+private void employeeName() {
+System.out.println("vikram");
+
+}
+		
+		
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+		Dhivya d=new Dhivya();
+		d.employeeName();
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
